24a9890d24d3c377ef595d0d19400a90a82ed5c3
ranselorm <ranselorm@outlook.com>
2026-05-04 17:11:24 +0100

    Implement Computer module with services and controller; add Power module and service

--- di/src/cpu/cpu.service.ts
+++ di/src/cpu/cpu.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class CpuService {}
+export class CpuService {
+  constructor(private powerService: PowerService) {}
+
+  compute(a: number, b: number) {
+    console.log('Drawing 10 watts of power');
+    this.powerService.supplyPower(12);
+    return a + b;
+  }
+}
--- di/src/disk/disk.service.ts
+++ di/src/disk/disk.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerService: PowerService) {}
+
+  getData() {
+    console.log('drawing 20 watts from power supply');
+
+    this.powerService.supplyPower(20);
+    return 'data';
+  }
+}
