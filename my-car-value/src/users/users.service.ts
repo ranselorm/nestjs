3c9d85eb6dd1ae09f978a1b55fab78f61fe9c299
ranselorm <ranselorm@outlook.com>
2026-05-05 12:26:49 +0100

    Implement user signup functionality with service integration

--- my-car-value/src/users/users.service.ts
+++ my-car-value/src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    const savedUser = this.repo.save(user);
+    console.log(`user created ${savedUser}`);
+    return savedUser;
+  }
+}
