778dd4b1c2347edb63851cae86cfa6d861067e58
ranselorm <ranselorm@outlook.com>
2026-05-07 20:03:13 +0100

    Refactor update and remove methods in UsersService for improved error handling and attribute assignment

--- my-car-value/src/users/users.service.ts
+++ my-car-value/src/users/users.service.ts
@@ -20,7 +20,24 @@ export class UsersService {
     return this.repo.find({ where: { email } });
   }
 
-  update(id: number, email: string) {
-    return this.repo.update(id, { email });
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new Error('user not found!');
+    }
+    Object.assign(user, attrs);
+
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new Error('user not found!');
+    }
+
+    return this.repo.remove(user);
   }
 }
