8cb64272bd73fa212f9bc32c2aede0febebe06f1
ranselorm <ranselorm@outlook.com>
2026-05-12 20:14:32 +0100

    Add AuthGuard to protect the whoAmI endpoint

--- my-car-value/src/users/users.controller.ts
+++ my-car-value/src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Delete,
   NotFoundException,
   Session,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -18,6 +19,7 @@ import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -28,6 +30,7 @@ export class UsersController {
   ) {}
 
   //get logged in user
+  @UseGuards(AuthGuard)
   @Get('/me')
   whoAmI(@CurrentUser() user: User) {
     return user;
