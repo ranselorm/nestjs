f114c8f2a2def5d5d72dd5d17bdb8dc9548a0083
ranselorm <ranselorm@outlook.com>
2026-05-12 19:45:12 +0100

    Implement CurrentUserInterceptor to set current user in request based on session

--- my-car-value/src/users/decorators/current-user.decorator.ts
+++ my-car-value/src/users/decorators/current-user.decorator.ts
@@ -4,6 +4,6 @@ export const CurrentUser = createParamDecorator(
   (data: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
     console.log(request.session);
-    return 'hi there!';
+    return request.currentUser;
   },
 );
---
+++ my-car-value/src/users/interceptors/current-user.interceptor.ts
@@ -0,0 +1,25 @@
+import {
+  ExecutionContext,
+  NestInterceptor,
+  Injectable,
+  CallHandler,
+} from '@nestjs/common';
+
+import { UsersService } from '../users.service';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class CurrentUserInterceptor implements NestInterceptor {
+  constructor(private usersService: UsersService) {}
+
+  async intercept(context: ExecutionContext, next: CallHandler) {
+    const request = context.switchToHttp().getRequest();
+    const { userId } = request.session || {};
+
+    if (userId) {
+      const user = await this.usersService.findOne(userId);
+      request.currentUser = user;
+    }
+    return next.handle();
+  }
+}
